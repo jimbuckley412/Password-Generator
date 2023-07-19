86b6adf8fd528505f577813be16a025213dc94e3
Jim Buckley <jimbuckley412@gmail.comgit config --global user.name jimgit config --global user.email jimbuckley412@gmail.com>
2023-07-19 14:56:12 -0400

    updated my script.js

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -7,7 +7,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789" + "!?@$%#"
 
+  for (let i = 1; i <= 10; i++) {
+    var char = Math.floor(math.random()
+                          * str.length +1);
+    
+      pass += str.charAt(char)
+  }
+
+  return pass;
+}
+function gfg_Run() {
+  el_down.innerHTML = generateP();
 }
 
 // Add event listener to generate button
