6030d8af077f2b75d7cf8fd19cefbc9c2a69cb74
Jim Buckley <jimbuckley412@gmail.comgit config --global user.name jimgit config --global user.email jimbuckley412@gmail.com>
2023-07-19 15:52:33 -0400

    correct javaScript added

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+  const specialCharacters = "?!@#$%";
+  const upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCases ="abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
 
 // Write password to the #password input
 function writePassword() {
@@ -7,20 +11,63 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789" + "!?@$%#"
-
-  for (let i = 1; i <= 10; i++) {
-    var char = Math.floor(math.random()
-                          * str.length +1);
-    
-      pass += str.charAt(char)
-  }
-
-  return pass;
-}
-function gfg_Run() {
-  el_down.innerHTML = generateP();
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passwordLength = prompt ("Please enter the number of characters you want for your password");
+  var numbers = confirm("Do you want numbersin your password?");
+  var lowerCases = confirm("Do you want lowercases in your password?");
+  var uppercases = confirm("Do you want uppercases in your password?");
+  var specialCharacters = confirm("Do you want special characters in your password?");
+  var minimumCount = 9;
+  var minimumNumbers ="1";
+  var minimumLowerCases = "6";
+  var minimumUpperCases = "1";
+  var minimumSpecialCharacters = "1";
+
+var functionArray = [
+  function getNumbers() {
+    return String.fromCharCode(math.floor(Math.random() * 10 + 48));
+  },
+  function getLowerCases() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+  },
+  function getUpperCases() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+  },
+  function getSpecialCharacters() {
+    return specialCharacters(Math.floor(Math.random() * specialCharacters.length));
+  }
+];
+
+if (numbers === true) {
+  minimumCount++;
+}
+
+if (lowerCases === true) {
+  minimumLowerCases = functionArray[]; 
+    minimumCount++;
+}
+
+if (specialCharacters === true) {
+  minimumSpecialCharacters = functionArray [3];
+  minimumCount++
+}
+
+var randomPasswordGenerated = "";
+for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+  var randomNumberPicked = Math.floor(Math.random() * 4);
+  randomPasswordGenerated += functionArray[randomNumberPicked]();
+}
+
+randomPasswordGenerated += minimumNumbers;
+randomPasswordGenerated += minimumLowerCases;
+randomPasswordGenerated += minimumUpperCases;
+randomPasswordGenerated += minimumSpecialCharacters;
+
+return randomPasswordGenerated;
+Event.preventDefault();
+}
