e2fd2e8c62ef7250629fdef7989ab321a363b93e
Jim Buckley <jimbuckley412@gmail.comgit config --global user.name jimgit config --global user.email jimbuckley412@gmail.com>
2023-07-26 14:09:21 -0400

    hopefully this works

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -1,80 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-  const generateButton = document.getElementById("generateBtn");
-  generateBtn.addEventListener("click", writePassword);
 
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-  var uppercase = confirm("Your password should have an uppercase letter! Click OK to continue");
-  var lowercase = confirm("Your password should have a lowercase letter! Click OK to continue");
-  var symbols = confirm("Your password should have a symbol! Click OK to continue");
-  var numbers = confirm("Your password should have a number! Click OK to continue");
-  var keyLength = prompt("Password must be between 8 and 128 characters! Click OK to continue");
-  var completedPassword = "";
-
-  var specialCharacters = "?!@#$%";
-  var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerCases ="abcdefghijklmnopqrstuvwxyz";
-  var numbers = "0123456789";
-
-  passwordText.value = password;
+  var specialCharacters = '?!@#$%'.split('');
+  var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  var lowerCases ="abcdefghijklmnopqrstuvwxyz".split('');
+  var pwNumbers = "0123456789".split('');
+  var passwordLength = 12;
+  var lettersMix = [...specialCharacters, ...upperCases, ...lowerCases, ...pwNumbers];
+  var characterLength = lettersMix.length;
+for(var i = 0; ((i<= passwordLength; i++) {
+  var randomNUmber = Math.floor(Math.random() * lettersMix.length);
+  password += lettersMix.substring(randomNumber, RandomNumber +1);
+}
+ document.querySelector("#generate").value = password;
 }
 
-function generatePassword() {
-  var minimumCount = 0;
-
-  var minimumNumbers ="";
-  var minimumLowerCases = "";
-  var minimumUpperCases = "";
-  var minimumSpecialCharacters = "";
-
-  var functionArray = [
-  function getNumbers() {
-    return String.fromCharCode(math.floor(Math.random() * 10 + 48));
-  },
-  function getLowerCases() {
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-  },
-  function getUpperCases() {
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-  },
-  function getSpecialCharacters() {
-    return specialCharacters(Math.floor(Math.random() * specialCharacters.length));
-  }];
-};
-
-if (numbers === true) {
-  minimumNumbers = functionArray.getNumbers();
-  minimumCount++;
-}
-
-if (lowerCases === true) {
-  minimumLowerCases = functionArray.getLowerCases(); 
-    minimumCount++;
-}
-if (upperCases === true) {
-  minimumUpperCases = functionArray.getUpperCases(); 
-    minimumCount++;
-}
-
-if (specialCharacters === true) {
-  minimumSpecialCharacters = functionArray.getSpecialCharacters();
-  minimumCount++
-}
-
-var randomPasswordGenerated = "";
-for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
-  var randomNumberPicked = Math.floor(Math.random() * 4);
-  randomPasswordGenerated += randomNumberPicked;
-  generatePassword();
-}
-
-randomPasswordGenerated += minimumNumbers;
-randomPasswordGenerated += minimumLowerCases;
-randomPasswordGenerated += minimumUpperCases;
-randomPasswordGenerated += minimumSpecialCharacters;
-
-return randomPasswordGenerated;
-generateBtn.addEventListener("click", generatePassword)
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
